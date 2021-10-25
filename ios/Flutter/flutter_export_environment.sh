c0560a47fd2314ccb2531cc2bbea34b62e173c16
Vivek Sonawane <viveksonawane600@gmail.com>
2021-10-25 08:39:43 +0530

    updated

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=G:\flutter\flutter"
-export "FLUTTER_APPLICATION_PATH=E:\AndroidStudioProjects\1stHacktoberfest"
+export "FLUTTER_ROOT=C:\flutter"
+export "FLUTTER_APPLICATION_PATH=E:\Vivek\Flutter Apps\1stHacktoberfest"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build\ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
