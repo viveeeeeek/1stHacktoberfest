b4283e9a775804c139f1d5d5b878a55ee0d4f26f
Safwan Mamji <safwanidrees957@gmail.com>
2022-10-17 01:05:27 +0500

    Fix username issue when loading image and button to open github user profile from detail page

---
+++ lib/utils/extension.dart
@@ -0,0 +1,10 @@
+extension GithubMakeUsername on String {
+  String toGithubUsername() {
+    String username = this;
+    List<String> splitName = this.split(" ");
+    if (splitName.length > 1) {
+      username = splitName[0] + splitName[1];
+    }
+    return username.toLowerCase().toString();
+  }
+}
